472c5e3d05e0496982c0fea4d215d1d628bd08d6
zhangyiming <2283942163@qq.com>
2017-12-22 16:36:45 +0800

    Initial Commit

---
+++ MMKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MMKitDemo
+//
+//  Created by 张一鸣 on 2017/12/22.
+//  Copyright © 2017年 张一鸣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
