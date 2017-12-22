72c51c102f357cf90b968c009ee0a3e563000b3f
zhangyiming <2283942163@qq.com>
2017-12-22 17:58:37 +0800

    add MMKit.h

---
+++ MMKit/MMKit.h
@@ -0,0 +1,21 @@
+//
+//  MMKit.h
+//  MMKitDemo
+//
+//  Created by 张一鸣 on 2017/12/22.
+//  Copyright © 2017年 张一鸣. All rights reserved.
+//
+
+#ifndef MMKit_h
+#define MMKit_h
+
+#import "MMPath.h"
+#import "MMFileManager.h"
+#import "MMFileReader.h"
+
+#import "MMLife.h"
+
+#import "MMDeviceInformation.h"
+
+#import "MMDebug.h"
+#endif /* MMKit_h */
